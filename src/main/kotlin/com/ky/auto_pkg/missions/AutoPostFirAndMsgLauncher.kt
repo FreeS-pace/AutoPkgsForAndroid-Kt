d0bc5648f7e6282c5f32f364a1c4bfd5d1504a1c
keepyoung <1203144058@qq.com>
2022-04-02 11:49:42 +0800

    1.测试Fir

--- src/main/kotlin/com/ky/auto_pkg/missions/AutoPostFirAndMsgLauncher.kt
+++ src/main/kotlin/com/ky/auto_pkg/missions/AutoPostFirAndMsgLauncher.kt
@@ -77,7 +77,7 @@ class AutoPostFirAndMsgLauncher private constructor() {
                         it.name.endsWith(".png")
                     }[0]
                 val imgBody: RequestBody =
-                    imgFile.asRequestBody("image/*".toMediaTypeOrNull())
+                    imgFile.asRequestBody("multipart/form-data".toMediaTypeOrNull())
                 val requestBody = MultipartBody.Builder()
                     .addFormDataPart("image_type", "message")
                     .addFormDataPart("image", imgFile.name, imgBody)
